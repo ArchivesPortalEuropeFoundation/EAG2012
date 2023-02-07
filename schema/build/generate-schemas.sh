da654864407f4ff36afc8f36010ff9ee4c84d58f
Kerstin Arnold <kerstin.arnold.berlin@gmail.com>
2023-02-08 00:53:46 +0100

    Update generate-schemas.sh
    Changed mentions of ead to mentions of eag

--- schema/build/generate-schemas.sh
+++ schema/build/generate-schemas.sh
@@ -11,15 +11,15 @@ echo "Getting started."
 
 java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it
 
-java -jar $jing -s ../src/modules/extensible-version/ead/ead-source.rng > ../xml-schemas/ead/ead.rng
+java -jar $jing -s ../src/modules/extensible-version/eag/eag-source.rng > ../xml-schemas/eag/eag.rng
 
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/ead/ead.rng
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eag/eag.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/eag/eag.rng
 
-java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/ead/ead.rng ../xml-schemas/ead/ead.xsd
+java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/eag/eag.rng ../xml-schemas/eag/eag.xsd
 
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/ead/ead.xsd
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eag/eag.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/eag/eag.xsd
 
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead.xsd -xsl:transformations/update-namespace-prefix-in-xsd.xsl -o:../xml-schemas/ead/ead.xsd
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eag/eag.xsd -xsl:transformations/update-namespace-prefix-in-xsd.xsl -o:../xml-schemas/eag/eag.xsd
 
 
 echo "All done."
